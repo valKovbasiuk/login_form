4fbfa79fe6906ccc37e115827c3c0e5f70fd716e
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-11-21 00:01:04 +0200

    working validation wo error message

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,8 +3,29 @@
   document
     .querySelectorAll("form")
     .forEach(element => element.setAttribute("novalidate", "true"));
+
+  const inputRequired = document.querySelectorAll("input[required]");
+
+  inputRequired.forEach(element =>
+    element.addEventListener("change", removeErrorClass)
+  );
+
+  function removeErrorClass(e) {
+    console.log(e.target.classList);
+    e.target.classList.remove(inputValidationErrorClass);
+  }
+
   // select target form
-  var dialog_wdw = document.querySelector(".modal");
+  const dialog_wdw = document.querySelector(".modal");
+
+  const submitButton = document.querySelector("form .submit");
+
+  // CSS class used to highlight input field did not pass validation
+  const inputValidationErrorClass = "error";
+
+  console.log(submitButton);
+
+  submitButton.addEventListener("click", submitForm);
 
   // select all buttons which can show/hide modal window
   document
@@ -16,5 +37,49 @@
     //   ? (dialog_wdw.style.display = "block")
     //   : (dialog_wdw.style.display = "none");
     dialog_wdw.classList.toggle("hidden");
+  }
+
+  function submitForm(e) {
+    e.preventDefault();
+    if (validateInput()) {
+      sendData();
+      submitButton.disabled = true;
+      console.log("success!!!");
+    }
+  }
+
+  function sendData() {}
+
+  function validateInput() {
+    validateEmail();
+    validatePassword();
+    return validateEmail() && validatePassword();
+  }
+
+  function validateEmail() {
+    const emailRe = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    const emailField = document.querySelector("input[type='email']");
+    if (
+      emailField.value.trim() === "" ||
+      !emailRe.test(emailField.value.trim())
+    ) {
+      emailField.classList.add(inputValidationErrorClass);
+      return false;
+    } else {
+      emailField.value = emailField.value.trim();
+      return true;
+    }
+  }
+
+  function validatePassword() {
+    const passField = document.querySelector("input[type='password']");
+    if (passField.value === "" || passField.value.length < 4) {
+      passField.classList.add(inputValidationErrorClass);
+      return false;
+    } else {
+      return true;
+    }
   }
 })();
