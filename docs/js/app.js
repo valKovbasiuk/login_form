a4e945e998fbf8a690109c8623c555110604ee26
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-11-21 17:15:21 +0200

    made validate on input

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,33 +1,35 @@
 (function() {
+  // const inputRequired = document.querySelectorAll("input[required]");
+
+  const emailField = document.querySelector("input[type='email'][required]");
+  const passField = document.querySelector("input[type='password'][required]");
+
+  // select target form
+  const dialog_wdw = document.querySelector(".modal");
+  const submitButton = document.querySelector("form .submit");
+  submitButton.disabled = true;
+
+  // CSS class used to highlight input field did not pass validation
+  const inputValidationErrorClass = "error";
+  const inputValidationValidClass = "valid";
+
   // turn off html validation when js can be executed
   document
     .querySelectorAll("form")
     .forEach(element => element.setAttribute("novalidate", "true"));
 
-  const inputRequired = document.querySelectorAll("input[required]");
+  emailField.addEventListener("input", function(e) {
+    // console.log("email input detected");
+    validateInput();
+  });
 
-  inputRequired.forEach(element =>
-    element.addEventListener("change", removeErrorClass)
-  );
-
-  function removeErrorClass(e) {
-    console.log(e.target.classList);
-    e.target.classList.remove(inputValidationErrorClass);
-  }
-
-  // select target form
-  const dialog_wdw = document.querySelector(".modal");
-
-  const submitButton = document.querySelector("form .submit");
-
-  // CSS class used to highlight input field did not pass validation
-  const inputValidationErrorClass = "error";
-
-  console.log(submitButton);
+  passField.addEventListener("input", function(e) {
+    // console.log("pass input detected");
+    validateInput();
+  });
 
   submitButton.addEventListener("click", submitForm);
 
-  // select all buttons which can show/hide modal window
   document
     .querySelectorAll(".btn_login, .btn__close")
     .forEach(element => element.addEventListener("click", classHideToggle));
@@ -44,6 +46,8 @@
     if (validateInput()) {
       sendData();
       submitButton.disabled = true;
+      emailField.value = "";
+      passField.value = "";
       console.log("success!!!");
     }
   }
@@ -53,32 +57,42 @@
   function validateInput() {
     validateEmail();
     validatePassword();
-    return validateEmail() && validatePassword();
+    if (validateEmail() && validatePassword()) {
+      submitButton.disabled = false;
+      return true;
+    } else {
+      submitButton.disabled = true;
+      return false;
+    }
   }
 
   function validateEmail() {
     const emailRe = new RegExp(
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
-    const emailField = document.querySelector("input[type='email']");
     if (
-      emailField.value.trim() === "" ||
+      // emailField.value.trim() === "" ||
       !emailRe.test(emailField.value.trim())
     ) {
-      emailField.classList.add(inputValidationErrorClass);
+      !emailField.classList.contains(inputValidationErrorClass) &&
+        emailField.classList.add(inputValidationErrorClass);
       return false;
     } else {
       emailField.value = emailField.value.trim();
+      emailField.classList.remove(inputValidationErrorClass);
+      emailField.classList.add(inputValidationValidClass);
       return true;
     }
   }
 
   function validatePassword() {
-    const passField = document.querySelector("input[type='password']");
-    if (passField.value === "" || passField.value.length < 4) {
-      passField.classList.add(inputValidationErrorClass);
+    if (passField.value.length < 4) {
+      !passField.classList.contains(inputValidationErrorClass) &&
+        passField.classList.add(inputValidationErrorClass);
       return false;
     } else {
+      passField.classList.remove(inputValidationErrorClass);
+      passField.classList.add(inputValidationValidClass);
       return true;
     }
   }
